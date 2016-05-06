001f89804aa260d3cb420102bf7f8d9178e0410c
zhuhaitong <zhuht@mediav.com>
2016-05-06 08:17:35 +0800

    temp commit

--- cpp/string/base64encode.cpp
+++ cpp/string/base64encode.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <sys/time.h>
 #include <stdint.h>
+#include "modp_b64.h"
 
 using namespace std;
 const int LOOP_COUNT = 100000;
@@ -103,12 +104,92 @@ std::string Base64Encode1(const std::string& src, std::string ending = "")
   return s;
 }
 
+std::string Base64Encode2(const std::string& src, std::string ending = "")
+{
+  uint8_t i = 0;
+  uint8_t b[4];
+  const uint8_t *bytes = (const uint8_t *) src.c_str();
+  uint32_t len = src.length();
+
+  std::string s;
+  //s.reserve(len*4/3);
+  std::cout << "begin:" << s.capacity() << std::endl;
+
+  while (len >= 3)
+  {
+    base64_encode(bytes, 3, b);
+    //for (i = 0; i < 4; i++)
+    //s.append(b[0], 3);
+    s.append(reinterpret_cast<char const*>(b),4);
+    bytes += 3;
+    len -= 3;
+  }
+
+  if (len)
+  {
+    base64_encode(bytes, len, b);
+    s.append(reinterpret_cast<char const*>(b),len+1);
+  }
+
+  s += ending;
+  std::cout << "end:" << s.capacity() << std::endl;
+
+  return s;
+}
+
+std::string Base64Encode3(const std::string& src, std::string ending = "")
+{
+  uint8_t i = 0;
+  uint8_t b[4];
+  const uint8_t *bytes = (const uint8_t *) src.c_str();
+  uint32_t len = src.length();
+
+  std::string s;
+
+  while (len >= 3)
+  {
+    base64_encode(bytes, 3, b);
+    s += b[0];
+    s += b[1];
+    s += b[2];
+    s += b[3];
+    bytes += 3;
+    len -= 3;
+  }
+
+  if (len > 0)
+  {
+    base64_encode(bytes, len, b);
+    s.append(reinterpret_cast<char const*>(b),len+1);
+  }
+
+  s += ending;
+
+  return s;
+}
+
+bool Base64EncodeModp(const std::string& input, std::string* output)
+{
+  std::string temp;
+  temp.resize(modp_b64_encode_len(input.size()));  // makes room for null byte
+
+  // null terminates result since result is base64 text!
+  int input_size = static_cast<int>(input.size());
+  int output_size= modp_b64_encode(&(temp[0]), input.data(), input_size);
+  if (output_size < 0)
+    return false;
+
+  temp.resize(output_size);  // strips off null byte
+  output->swap(temp);
+  return true;
+}
+
 int main(int argc, char *argv[])
 {
   std::string s = "zqus387489tid'23	tck=e02fce03a933b5c8fffdf46442a2e8e9ts"
     "1461052882959uunid4tid=23&tck=e02fce03a933b5c8fffdf46442a2e8e9&ts=1461052882959&ver=1ip:106.39.88.82hostname:tf41dg.prod.mediav.comagent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4)"
     "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36lang:en,zh-CN;q=0.8,zh;q=0.6,zh-TW;q=0.4referer:http://ckmap.mediav.com/b?type=10proxy:106.39.88.82, 202.79.203.111_mvctn191270=_mvsrc=118216_523698_1043440&_mvcam=191270_1241961_11970810_58740969_0&osr=oqdT0qhmgzj0&time=1449650466&rdom=anonymous; _mvctn165564=_mvsrc=118478_524001_1044112&_mvcam=165564_1224948_11775153_58841999_0&osr=Zg4c0cS49V00&time=1451040575&rdom=anonymous; v=)he((]^XdqB2fOE=[AjT; _jzqa=1.4336274511317232000.1447337529.1447337529.1461045516.2; _jzqc=1; _jzqckmp=1; ckmts=PUPtXAzi,P6PtXAzi,RGPtXAzi,R6PtXAzi,U6PtXAzi,JGPtXAzi,JrPtXAzi,J6PtXAzi,bUPtXAziver10:0|0:0|0:0|0:10p!"
-    "version1.0";
+    "version1.01";
   string a;
   struct timeval start, end;
   gettimeofday(&start, NULL);
@@ -127,6 +208,36 @@ int main(int argc, char *argv[])
     b = Base64Encode1(s);
   }
   std::cout << "base64encoded: " << b << std::endl;
+  gettimeofday(&end, NULL);
+  std::cout << " Base64Encode1 timeuse: " << 1000000*(end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) << std::endl;
+
+  string e;
+  gettimeofday(&start, NULL);
+  for (int i = 0; i < LOOP_COUNT; ++i)
+  {
+    e = Base64Encode2(s);
+  }
+  std::cout << "base64encoded: " << e << std::endl;
+  gettimeofday(&end, NULL);
+  std::cout << " Base64Encode2 timeuse: " << 1000000*(end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) << std::endl;
+
+  string f;
+  gettimeofday(&start, NULL);
+  for (int i = 0; i < LOOP_COUNT; ++i)
+  {
+    f = Base64Encode3(s);
+  }
+  std::cout << "base64encoded: " << e << std::endl;
+  gettimeofday(&end, NULL);
+  std::cout << " Base64Encode3 timeuse: " << 1000000*(end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) << std::endl;
+
+  string c;
+  gettimeofday(&start, NULL);
+  for (int i = 0; i < LOOP_COUNT; ++i)
+  {
+    Base64EncodeModp(s, &c);
+  }
+  std::cout << "base64encoded: " << c << std::endl;
   gettimeofday(&end, NULL);
   std::cout << " Base64Encode1 timeuse: " << 1000000*(end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) << std::endl;
   return 0;
